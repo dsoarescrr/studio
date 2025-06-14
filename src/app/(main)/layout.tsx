4f1001b0e853b9743933fe4094d68fc00c12765e
Diogo Soares <soparajogosxd999@gmail.com>
2025-06-14 22:38:31 +0000

    quando a aplicação abre o mapa fica mais para baixo porque a pagina dá r

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-hidden"> {/* Added overflow-hidden */}
       <UserProfileHeader /> {/* Added UserProfileHeader here */}
       <main className="flex-1 flex flex-col pt-16 pb-[var(--bottom-nav-height)]"> {/* Added pt-16 for UserProfileHeader */}
         {children}
