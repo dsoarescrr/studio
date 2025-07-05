936dafb735e5d57175f36930989621d1feee4c40
Diogo Soares <soparajogosxd999@gmail.com>
2025-07-05 00:09:22 +0000

    nao mudou nada, o fundo a volta do mapa continua igual

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -322,7 +322,7 @@ const SidebarInset = React.forwardRef<
     <main
       ref={ref}
       className={cn(
-        "relative flex h-full flex-1 flex-col bg-background",
+        "relative flex h-full flex-1 flex-col",
         "peer-data-[variant=inset]:h-[calc(100%-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
