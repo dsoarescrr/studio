57b1c41ee3e5cfc76fe77d9a5f0c9c0f12cc3fe9
Diogo Soares <soparajogosxd999@gmail.com>
2025-07-05 01:02:33 +0000

    como podes analisar na imagem anexada a barra de cima com as informaçoes

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -234,7 +234,7 @@ const Sidebar = React.forwardRef<
         />
         <div
           className={cn(
-            "duration-200 fixed inset-y-0 z-10 hidden h-full w-[--sidebar-width] transition-[left,right,width] ease-linear md:flex",
+            "duration-200 fixed top-16 bottom-[var(--bottom-nav-height)] z-10 hidden h-auto w-[--sidebar-width] transition-[left,right,width] ease-linear md:flex",
             side === "left"
               ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
               : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
